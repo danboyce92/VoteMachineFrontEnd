46ff712115811f85425705909a3dd1ec0a03a302
Daniel Boyce <daarf92@gmail.com>
2022-10-12 15:41:28 +0100

    Connect button works, next add admin functionality

--- votemachine/src/App.js
+++ votemachine/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ethers } from 'ethers';
 import './styles/App.css';
 import Register from './components/Register';
 import Admin from './components/Admin';
@@ -8,17 +9,62 @@ import Results from './components/Results';
 
 
 function App() {
+ 
+  const [publicKey, setPublickey] = useState();
+
+  const [network, setNetwork] = useState();
+
+  const [chainId, setChainId] = useState();
+
+  const [msg, setMsg] = useState();
+
+
+  const connectButton = async () => {
+    const { ethereum } = window;
+    if (ethereum.isMetaMask) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const accounts = await provider.send("eth_requestAccounts", []);
+      const { name, chainId } = await provider.getNetwork();
+      setNetwork(name);
+      setChainId(chainId);
+      setPublickey(accounts[0]);
+    } else {
+      setMsg("Install MetaMask");
+    }
+  };
+
+  const connectWallet = () => {
+    if (!publicKey) {
+      return  "Connect Wallet"
+    } else return  "Connected"
+    
+  }
+  
+  
+  
+  
   return (
     <div className="App">
 
       <div className="parent">
+        <div className="info">
+          Public key: {publicKey} 
+          <br></br>
+          Network: {network}
+          <br></br>
+          Chain ID: {chainId}
+        </div>
 
       <div className="conBut">
-        <button className="ui large inverted green button">Connect Wallet</button>
+        <button className="ui large inverted green button"
+        onClick={connectButton}
+        >{connectWallet()}</button>
+
       </div>
 
       <div className="title">
             The Vote Machine
+            
           </div>
 
         <div className="child">
